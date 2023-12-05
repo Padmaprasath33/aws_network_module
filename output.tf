b53d146eb9eb5a2bc14f169bcea2769b73d92664
Padmaprasath <padmaprasath33@gmail.com>
2023-12-05 09:32:58 +0530

    Added S3 resources

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "vpc_cidr_block" {
   value = aws_vpc.main.cidr_block
 }
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
 output "aws_subnet_public" {
   value = aws_subnet.public_subnet[*].id
 }
